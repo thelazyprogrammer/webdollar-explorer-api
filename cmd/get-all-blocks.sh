75beeadcb7cd5a2a3da550b1601f929ce9622904
ader1990 <webmaster@lazyprogrammer.io>
2018-05-06 17:35:35 +0000

    [block] Updated block getter to return valid json

--- cmd/get-all-blocks.sh
+++ cmd/get-all-blocks.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 BLOCK_START=0
-BLOCK_END=205601
+BLOCK_END=2000
+
+echo '['
 
 for((block=$BLOCK_START;block<$BLOCK_END;block++))
 do
     output=$(curl -sb -H "Accept: application/json" "http://localhost:3000/block/$block")
-    echo ${output} | jq '.' | grep 'trxs' | grep -v '"trxs": \[\]'
+    out=$(echo ${output} | jq '.' | grep 'trxs' | grep -v '"trxs": \[\]')
     if [ $? -eq 0 ]; then
-        printf "\n\n<<< Getting block number $block \n"
         echo ${output} | jq '.'
+        echo ","
     fi
     
 done
-
+echo "]"
 
