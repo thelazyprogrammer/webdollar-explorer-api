e35493bae644cb27f21b06e081d80522d127c55c
ader1990 <webmaster@lazyprogrammer.io>
2018-05-15 14:00:00 +0000

    [client] Fixed search by reloading page

--- client/src/components/utils/Search.vue
+++ client/src/components/utils/Search.vue
@@ -2,7 +2,7 @@
 
   <div id="search_wrapper">
     <input v-model="searchAddress" id="searchBox" v-on:enter="onSearchAddress" type="text" placeholder="Search by Address/Block" autocomplete="off">
-    <button v-model="searchStart" v-on:click="onSearchAddress" type="submit">GO</button>
+    <button v-on:click="onSearchAddress" type="submit">GO</button>
   </div>
 
 </template>
@@ -13,43 +13,25 @@ import BlocksService from '@/services/BlocksService'
 
 export default {
 
-  name: 'miner',
+  name: 'search',
 
   data () {
     return {
-      miner: [],
-      searchAddress: '',
-      searchStart: ''
+      searchAddress: ''
     }
-  },
-
-  beforeRouteUpdate (to) {
-    this.getMiner(to.params.miner_address)
-  },
-
-  mounted () {
-    this.getMiner(window.location.href.substring(34,window.location.href.length))
   },
 
   methods: {
 
-    async getMiner (miner) {
-      this.miner = [];
-      const response = await BlocksService.fetchMiner(miner);
-      this.miner = response.data;
-    },
-
     async onSearchAddress (event) {
-
-      if (this.searchAddress.indexOf("WEBD")>=0){
-        await this.getMiner(this.searchAddress);
-        this.$router.push({ path: `/miner/`+this.miner.address });
-      }else{
-        this.$router.push({ path: `/blocks/`+this.searchAddress });
+      if (this.searchAddress) {
+        if (this.searchAddress.indexOf("WEBD")>=0){
+          this.$router.push({ path: `/miner/` + this.searchAddress });
+        } else {
+          this.$router.push({ path: `/blocks/` + this.searchAddress });
+        }
+        location.reload()
       }
-
-      this.searchAddress = '';
-
     }
 
   }
