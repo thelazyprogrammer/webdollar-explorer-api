4e9cd5e0c665b2843fd46f43a46c0084d69c5b8e
ionutAlexandruPanait <ionutalexpanait@gmail.com>
2018-05-15 06:16:01 +0300

    new design

---
+++ client/src/components/pages/LastBlocksMined.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <div class="container">
+
+    <h2>Last mined blocks</h2>
+
+    <blocks-list :blocks="this.blocks"></blocks-list>
+
+  </div>
+
+</template>
+
+<script>
+
+import BlocksList from '@/components/lists/LightMinedBlocks.vue'
+import BlocksService from '@/services/BlocksService'
+
+export default {
+
+  name: 'last mined blocks',
+
+  components: {BlocksList},
+
+  data () {
+    return {
+      blocks: []
+    }
+  },
+
+  mounted () {
+    this.getBlocks()
+  },
+
+  methods: {
+    async getBlocks () {
+      const response = await BlocksService.fetchBlocks()
+      this.blocks = response.data
+    }
+  }
+
+}
+</script>
+
+<style type="text/css">
+
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Blocks from '@/components/Blocks'
-import Block from '@/components/Block'
-import Miner from '@/components/Miner'
-import Search from '@/components/Search'
+import Blocks from '@/components/pages/LastBlocksMined'
+import Block from '@/components/pages/Block'
+import Miner from '@/components/pages/Miner'
+import Search from '@/components/utils/Search'
 
 Vue.use(Router)
 
