f010e73a169de7280712c18184784f844dcb8481
ader1990 <webmaster@lazyprogrammer.io>
2018-07-02 08:17:49 +0000

    client: pools: Added payout and fixed css

--- client/src/components/pages/Pools.vue
+++ client/src/components/pages/Pools.vue
@@ -2,21 +2,17 @@
   <div class="pools">
   <h2> Public Pools for WebDollar </h2>
 
-  <table class="transactionsTable">
+  <table class="transactionsTable" style="font-size:0.84em;">
 
     <tr>
-      <td>No.</td>
       <td>Name</td>
       <td>Address</td>
-      <td>Status</td>
+      <td>MinPayout</td>
       <td>Fee</td>
+      <td>Status</td>
     </tr>
 
     <tr v-for="(pool, index) in pools">
-
-      <td align="left">
-       {{ index}}
-      </td>
 
       <td align="left">
        <a :href="'#/miner/' + pool.address">{{ pool.name }} </a>
@@ -27,16 +23,20 @@
       </td>
 
       <td align="left" style="text-align:center">
-       {{ pool.status }}
+       {{ pool.min_amount }} WEBD
       </td>
 
       <td align="left" style="text-align:center">
        {{ pool.fee }}%
       </td>
+
+      <td align="left" style="text-align:center">
+       {{ pool.status }}
+      </td>
    </tr>
   </table>
 
-  <p class="transactionsTable" style="width:100%;max-width: 680px; margin: 0 auto; padding: 10px; margin-top:30px;"> The pool information does not and will not contain its access link, as it might change over time. If one prefers to have its pool information available here, please file a Pull Request to the GitHub repository <a href="https://github.com/thelazyprogrammer/webdollar-explorer-api">here</a>.</p> 
+  <p class="transactionsTable" style="width: 680px; margin: 0 auto; padding: 10px; margin-top:30px;"> The pool information does not and will not contain its access link, as it might change over time. If one prefers to have its pool information available here, please file a Pull Request to the GitHub repository <a href="https://github.com/thelazyprogrammer/webdollar-explorer-api">here</a>.</p> 
   </div>
 
 </template>
@@ -52,16 +52,18 @@ export default {
     return {
       pools: [
         {
-          name: 'WebdDollar Experimental Pool 1',
+          name: 'Experimental Pool 1',
           address: 'WEBD$gDU+tP3@42@L9$Is463vDJi4IKrabPNNn$$',
-          status: 'experimental',
-          fee: 2
+          status: 'up',
+          fee: 2,
+          min_amount: 10
         },
         {
-          name: 'WebdDollar Experimental Pool 2',
+          name: 'Experimental Pool 2',
           address: 'WEBD$gCGdPQLohdeHQqa+ptoJ@QK2LCvN7IS5JX$',
-          status: 'experimental',
-          fee: 2
+          status: 'up',
+          fee: 2,
+          min_amount: 10
         },
       ]
     }
