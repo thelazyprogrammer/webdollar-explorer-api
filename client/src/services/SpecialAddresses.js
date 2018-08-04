5880e70c78da8df0245ea5491052b36461b7e241
Ionut Moraru <morion4000@gmail.com>
2018-08-04 12:27:45 +0300

    Update SpecialAddresses.js

--- client/src/services/SpecialAddresses.js
+++ client/src/services/SpecialAddresses.js
@@ -23,7 +23,7 @@ export default {
        min_amount: 20
      },
      {
-       name: 'WMP_1',
+       name: 'WMP',
        address: 'WEBD$gCrEhDsa9Wv$@x3QkNd4jbNcb5bISk8Nyv$',
        'status': 'up',
        fee: 2,
@@ -41,13 +41,6 @@ export default {
        address: 'WEBD$gCsh0nNrsZv9VYQfe5Jn$9YMnD4hdyx62n$',
        'status': 'up',
        fee: 1,
-       min_amount: 20
-     },
-     {
-       name: 'WMP_2',
-       address: 'WEBD$gAPa@tBU41xAyP6WuFVB79PLGqZHIh48K7$',
-       'status': 'down',
-       fee: 2,
        min_amount: 20
      },
      {
