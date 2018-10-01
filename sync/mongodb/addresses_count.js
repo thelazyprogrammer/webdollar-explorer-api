7e67c928845d8ba3d5da3ab1426671df93a57b8f
ader1990 <webmaster@lazyprogrammer.io>
2018-10-01 22:04:02 +0300

    syncer: mongodb: query for number of addresses

---
+++ sync/mongodb/addresses_count.js
@@ -0,0 +1,34 @@
+[
+  {
+    $group: {
+      _id: 0,
+      addresses: {
+        $addToSet: "$addresses"
+      }
+    }
+  }, {
+    $unwind: {
+      path: '$addresses',
+      preserveNullAndEmptyArrays: false
+    }
+  }, {
+    $unwind: {
+      path: '$addresses',
+      preserveNullAndEmptyArrays: false
+    }
+  }, {
+    $group: {
+      _id: 0,
+      addresses: {
+        $addToSet: "$addresses"
+      }
+    }
+  }, {
+    $project: {
+      _id: 0,
+      addresses_count: {
+        $size: "$addresses"
+      }
+    }
+  }
+]
