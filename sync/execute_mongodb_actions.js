5202beb2f0fea50be93d7c708bf7624421127e83
lazy programmer <webmaster@lazyprogrammer.io>
2019-02-11 21:05:03 +0000

    sync: add check sanity for blockchain

--- sync/execute_mongodb_actions.js
+++ sync/execute_mongodb_actions.js
@@ -19,7 +19,8 @@
 const atob = require('atob'),
   bs58 = require('bs58'),
   crypto = require('crypto'),
-  winston = require('winston');
+  winston = require('winston'),
+  requestPromise = require('request-promise');
 
 const MongoClient = require('mongodb').MongoClient;
 
@@ -103,6 +104,39 @@ async function getPoSMiners() {
   return posMiners
 }
 
+async function checkSanity() {
+  let knownAddresses = [
+    'WEBD$gAY19Mp6VnujexNW3gjHNiAEQkvkIs9div$',
+    'WEBD$gCsh0nNrsZv9VYQfe5Jn$9YMnD4hdyx62n$',
+    'WEBD$gCMxAKX96yhmaygo@NG+vnb4cz1eYoYpMv$',
+    'WEBD$gAosJ5+2tFnS$q5EsZZGVHKEU#cHnPJbZz$'
+  ]
+
+  let failure = false
+  for (let i in knownAddresses) {
+    let address = knownAddresses[i]
+    let optionsExplorer = {
+      uri:  'http://localhost:3000' + '/address/' + encodeURIComponent(address),
+      json: true
+    };
+    var optionsCore = {
+      uri:  'http://localhost:4444' + '/address/balance/' + encodeURIComponent(address),
+      json: true
+    };
+
+    let balanceExplorer = await requestPromise(optionsExplorer)
+    let balanceCoreWebdollar = await requestPromise(optionsCore)
+    if (balanceExplorer.balance != balanceCoreWebdollar.balance) {
+      failure = true
+      console.log(address + ": balances are different. explorer " + balanceExplorer.balance + " != core " + balanceCoreWebdollar.balance)
+    } else {
+      console.log(address + ": balances are the same. explorer " + balanceExplorer.balance + " == core " + balanceCoreWebdollar.balance)
+    }
+  }
+   if (failure) {
+     throw "Addresses did not pass sanity check"
+   }
+}
 
 async function executeCommand(command) {
   if (command == "get_pos_miners") {
@@ -116,6 +150,8 @@ async function executeCommand(command) {
     console.log("master resolver resolved: " + miners[0].balance_pos)
     console.log("webd staked during the period: " + (totalMined * 1888608034 / miners[0].balance_pos))
     return
+  } else if (command == "check_sanity") {
+    await checkSanity()
   } else {
     throw ("Command not available: " + command)
   }
