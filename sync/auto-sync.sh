87c5637a4282a697f25dbcc8f57223f53081c0f5
ader1990 <webmaster@lazyprogrammer.io>
2019-02-11 12:21:22 +0100

    sync: added auto-sync.sh script

---
+++ sync/auto-sync.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+BLOCKCHAIN_PATH=~/webdollar/Node-WebDollar/blockchainDB3
+TIMEOUT=36000
+
+function sync() {
+    echo "Syncing..."
+    rm -rf "blockchainDB3"
+    cp -r $BLOCKCHAIN_PATH .
+    node pouchdb-mongodb-syncer.js 590000 1000000 force
+}
+
+while true; do
+    sync
+    sleep $TIMEOUT
+done
+
