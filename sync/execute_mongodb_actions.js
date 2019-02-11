74cd3ebbc326546c1d8b2d4687553601178b0542
lazy programmer <webmaster@lazyprogrammer.io>
2019-02-11 07:40:03 +0000

    sync: implement get_pos_miners action

---
+++ sync/execute_mongodb_actions.js
@@ -0,0 +1,125 @@
+//
+// how to install mongodb
+// https://www.digitalocean.com/community/tutorials/how-to-install-mongodb-on-ubuntu-16-04
+//
+// pouchdb does not need to be installed
+// blockchainDB3 needs to be in the folder
+//
+// Example to sync blocks from 0 to 1000
+//   # install mongodb
+//   npm install
+//   node pouchdb-mongodb-syncer.js 0 1000
+//
+// It syncs only the block information to the
+// blocks collections in the MongoDB blockchainDB3 database
+//
+// To do:
+//   1. If a block has transactions, compute involved_addresses and fee fields
+//   2. Create transactions collection
+const atob = require('atob'),
+  bs58 = require('bs58'),
+  crypto = require('crypto'),
+  winston = require('winston');
+
+const MongoClient = require('mongodb').MongoClient;
+
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.File({ filename: __filename + '.log' })
+  ]
+});
+
+const mongodbBlockchainDB = "blockchainDB3"
+const mongodbBlockCollection = "blocks"
+const mongodbTransactionCollection = "transactions"
+const mongodbMTransactionCollection = "mtransactions"
+const mongodbUrl = "mongodb://localhost:27017"
+
+let miner = {
+  address: '',
+  pos_blocks: 0,
+  ratio: 0,
+  from: '',
+  to: '',
+  days: 0
+}
+
+async function getPoSMiners() {
+  let posMiners = []
+  let mongoDB = await MongoClient.connect(mongodbUrl, { useNewUrlParser: true })
+  let blockChainDB = mongoDB.db(mongodbBlockchainDB);
+  let toTimestamp = new Date()
+  toTimestamp = toTimestamp.getTime() / 1000 - 60 * 60 * 0 
+  let fromTimestamp = toTimestamp - 60 * 60 * 24 * 100
+  let miners = await blockChainDB.collection(mongodbBlockCollection).aggregate([
+    {
+        '$match': {
+          number: {
+            '$gte': 567810
+          },
+          algorithm: 'pos',
+          timestamp:  { $gte: fromTimestamp, $lte: toTimestamp }
+        },
+    },
+    {
+          '$group': {
+            '_id': 0, 
+            'resolvers': {
+              '$addToSet': '$resolver'
+            }
+          }
+    }
+  ]).toArray()
+  if (miners.length > 0) {
+    miners = miners[0].resolvers
+  }
+  for (miner_index in miners) {
+    let miner_balance_pos = await blockChainDB.collection(mongodbBlockCollection).aggregate([
+      { $match: {
+           resolver: miners[miner_index],
+           algorithm: 'pos',
+           reward: { $gt: 0 },
+           number: { $gte: 567810 },
+           timestamp:  { $gte: fromTimestamp, $lte: toTimestamp }
+        }
+      },
+      { $group: {
+          _id: 1,
+          balance: {
+             $sum: "$reward"
+          }
+        }
+      }
+     ]).toArray()
+     if (miner_balance_pos.length == 1) {
+     posMiners.push(
+       {
+         address: miners[miner_index],
+         balance_pos: miner_balance_pos[0].balance / 10000
+       }
+     )}
+  }
+  return posMiners
+}
+
+
+async function executeCommand(command) {
+  if (command == "get_pos_miners") {
+    let miners = await getPoSMiners()
+    miners = miners.sort((a,b) => b.balance_pos - a.balance_pos)
+    let totalMined = 0
+    miners.forEach(function (m) { totalMined+= m.balance_pos})
+    console.log("pos miners: " + miners.length)
+    console.log("pos resolved amount: " + totalMined)
+    console.log("master resolver balance: " + 1888608034)
+    console.log("master resolver resolved: " + miners[0].balance_pos)
+    console.log("webd staked during the period: " + (totalMined * 1888608034 / miners[0].balance_pos))
+    return
+  } else {
+    throw ("Command not available: " + command)
+  }
+}
+
+executeCommand(process.argv[2])
+return
