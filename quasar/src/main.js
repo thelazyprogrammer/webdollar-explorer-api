bf66967fc28cdc644b049b885ed3aad1caaf71ee
lazy programmer <webmaster@lazyprogrammer.io>
2019-02-22 21:26:13 +0000

    first quasar commit

---
+++ quasar/src/main.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import './styles/quasar.styl'
+import 'quasar/dist/quasar.ie.polyfills'
+import lang from 'quasar/lang/en-us.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
+import '@quasar/extras/mdi-v3/mdi-v3.css'
+import '@quasar/extras/eva-icons/eva-icons.css'
+import {
+  Quasar, 
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel,
+} from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel,
+  },
+  directives: {
+  },
+  plugins: {
+  },
+  lang: lang
+ })
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
