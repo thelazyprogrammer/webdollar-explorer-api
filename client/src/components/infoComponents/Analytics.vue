4f5445885c2ad9489821fba03f263318bb09fd98
lazyprogrammer <webmaster@lazyprogrammer.io>
2019-04-18 01:32:17 +0000

    client: added analytics component

---
+++ client/src/components/infoComponents/Analytics.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <highcharts class="tabWrapper" :options="chart_options"></highcharts>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'analytics',
+
+  components: { },
+
+  props: {
+    chart_options: {}
+  },
+  methods: {
+  }
+}
+</script>
+<style type="text/css">
+
+</style>
